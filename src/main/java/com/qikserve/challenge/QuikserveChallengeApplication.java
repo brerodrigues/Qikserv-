085e57598b85b66adf7c8024fe464ad2418860c1
Breno Rodrigues da Silva <brenos@cpqd.com.br>
2024-04-21 19:10:13 -0300

    starting project

---
+++ src/main/java/com/qikserve/challenge/QuikserveChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.qikserve.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikserveChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikserveChallengeApplication.class, args);
+	}
+
+}
